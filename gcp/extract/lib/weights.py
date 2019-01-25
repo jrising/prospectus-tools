f823d1ee79b85d197431b32d4b8b3fcefc351938
James Rising <jarising@gmail.com>
2019-01-25 10:34:54 -0800

    add mean

--- gcp/extract/lib/weights.py
+++ gcp/extract/lib/weights.py
@@ -105,7 +105,7 @@ class WeightedECDF(StepFunction):
     @staticmethod
     def encode_evalqvals(evalqvals):
         encoder = {'mean': 2}
-        return map(lambda p: p if isinstance(p, float) else encoder[p], pp)
+        return map(lambda p: p if isinstance(p, float) else encoder[p], evalqvals)
 
 if __name__ == '__main__':
     import sys
