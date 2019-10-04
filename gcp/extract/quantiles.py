95eea2a9cf86cf3fe155b47fd7e07d5becacb11e
Maya Norman <manorman@battuta.berkeley.edu>
2019-10-04 12:30:18 -0700

    added in the following logic: if a key (rcp,ssp) or (region, ssp) which exists in the point estimate output does not exist in the delta method output do not output a file for the key and skip to the next iteration of the loop

--- gcp/extract/quantiles.py
+++ gcp/extract/quantiles.py
@@ -42,6 +42,12 @@ if configs.is_parallel_deltamethod(config):
 
 for filestuff in data:
     print "Creating file: " + str(filestuff)
+    if configs.is_parallel_deltamethod(config):
+        try:
+            output_check = parallel_deltamethod_data[filestuff].keys()
+        except:
+            print str(filestuff) + " is not in delta method output. Skipping model specification..."
+            continue
     with open(configs.csv_makepath(filestuff, config), 'w') as fp:
         writer = csv.writer(fp, quoting=csv.QUOTE_MINIMAL)
         rownames = configs.csv_rownames(config)
